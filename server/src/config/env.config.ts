4b58eee3f573e90735c3ed5a8e311f74e65054cf
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-08-05 23:11:21 +0530

    set uo config:

---
+++ server/src/config/env.config.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT:string= process.env.PORT || '8006';
+const DBURI:string= process.env.DBURI || "mongodb://localhost:27017/studentDb" ;
+const ACCESS_TOKEN_SECRET:string|undefined= process.env.ACCESS_TOKEN_SECRET;    
+const EMAIL:string | undefined=process.env.EMAIL; 
+const PASS:string |undefined = process.env.PASS;
+const ADMIN_PASS:string |undefined = process.env.ADMIN_PASS;    
+const ADMIN_ID:string |undefined = process.env.ADMIN_ID;
+
+export {PORT,DBURI, ACCESS_TOKEN_SECRET, EMAIL, PASS, ADMIN_PASS, ADMIN_ID}
