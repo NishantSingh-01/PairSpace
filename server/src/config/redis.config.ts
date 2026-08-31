acb7319ca6e2a0bb125475b0d0d61606bdee2522
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-08-31 20:55:48 +0530

    redis setup

---
+++ server/src/config/redis.config.ts
@@ -0,0 +1,19 @@
+import { createClient } from "redis"
+
+const redisClient = createClient({
+    url: "redis://localhost:6379"
+})
+
+
+redisClient.on("error", (err: any) => {
+    console.log("Redis Error", err)
+})
+
+export const connectRedis = async () => {
+    if (!redisClient.isOpen) {
+        await redisClient.connect()
+        console.log("Redis Connected")
+    }
+}
+
+export default redisClient
